8d6acd6740d8fb8171df102270af8e179502b72d
Stanislav Kozin <stas@staskozin.ru>
2021-05-03 22:50:16 +0300

    Добавил получение фильма по id Кинопоиска

--- endpoint/movie.js
+++ endpoint/movie.js
@@ -1,10 +1,25 @@
 const { db } = require('../lib/db');
 
 async function getAllMovies() {
-  const movies = await db.query('SELECT * FROM movie')
-  return movies.rows;
+  const movies = await db.query('SELECT * FROM movie');
+  return movies.rows.map((movie) => {
+    return {
+      ...movie,
+      age_restriction: Number(movie.age_restriction)
+    };
+  });
+}
+
+async function getMovie(id) {
+  const movie = await db.query('SELECT * FROM movie WHERE movie_id = $1', [id]);
+  if (movie.rows.length === 0) return null;
+  return {
+    ...movie.rows[0],
+    age_restriction: Number(movie.rows[0].age_restriction)
+  }
 }
 
 module.exports = {
-  getAllMovies
+  getAllMovies,
+  getMovie
 };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const restify = require('restify');
-const { getAllMovies } = require('./endpoint/movie');
+const { getAllMovies, getMovie } = require('./endpoint/movie');
 
 const wrap = function (fn) {
   return function (req, res, next) {
@@ -15,6 +15,17 @@ server.get('/movie', wrap(async (req, res) => {
   const movies = await getAllMovies();
   res.header('content-type', 'json');
   res.send(movies);
+}));
+
+server.get('/movie/:id', wrap(async (req, res) => {
+  const movie = await getMovie(req.params.id);
+  res.header('content-type', 'json');
+  if (movie === null) {
+    res.status(404);
+    res.send({ message: 'Фильм не найден' });
+  } else {
+    res.send(movie);
+  }
 }));
 
 server.listen(3001, '127.0.0.1');
